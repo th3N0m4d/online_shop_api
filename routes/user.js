dfdc3890093bc9421f5b9d68c9bc2373a9d674e5
th3N0m4d <edielton.dantas@hotmail.com>
2020-02-19 17:47:01 -0300

    Create basic user route's skeleton

---
+++ routes/user.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import controller from '../controllers/user'
+
+const router = express.Router()
+
+router.route('/')
+  .get(controller.list)
+  .post(controller.create)
+
+router.route('/:id')
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.remove)
+
+router.param('id', controller.userById)
+
+export default router
