15405395a028c3e41f835d09577364722eebcaeb
th3N0m4d <edielton.dantas@hotmail.com>
2020-02-19 16:50:17 -0300

    Separate app from server

--- app.js
+++
@@ -1,5 +0,0 @@
-import fashiop from './fashiop'
-
-const port = process.env.PORT
-
-fashiop.listen(port, () => console.log(`Server is running on port ${port}`))
--- fashiop.js
+++ fashiop.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 
+import userRoutes from './routes/user'
+
 dotenv.config()
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello world!' })
-})
+app.use(express.json())
+
+app.use('/api/user', userRoutes)
 
 export default app
