c3c773dfd01d32f7a1563157dfefe19ad6405d5e
th3N0m4d <edielton.dantas@hotmail.com>
2020-03-16 15:12:47 +0100

    Connect to mongodb

--- fashiop.js
+++ fashiop.js
@@ -1,11 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 import userRoutes from './routes/user'
 
 dotenv.config()
 
 const app = express()
+const uri = process.env.ATLAS_URI
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+
+const connection = mongoose.connection
+
+connection.once('open', () => {
+  console.info('MongoDB database connection established successfully')
+})
 
 app.use(express.json())
 
