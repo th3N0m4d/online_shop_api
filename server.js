a2b22048efa3b7fde6e7fa200bf402d04b3598de
th3N0m4d <edielton.dantas@hotmail.com>
2020-03-21 13:26:17 +0100

    Move mongoose's setup to server.js

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
 import fashiop from './fashiop'
+
+dotenv.config()
+
+const uri = process.env.ATLAS_URI
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+
+const connection = mongoose.connection
+
+connection.once('open', () => {
+  console.info('MongoDB database connection established successfully')
+})
 
 const port = process.env.PORT
 
